ad5cb728375888ebe549f3b06ff43207ce66bbd5
hdisma <ihdominguez905@gmail.com>
2021-03-30 00:19:33 -0400

    Add project files.

---
+++ src/Tavern.IdentityServer/Config.cs
@@ -0,0 +1,28 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tavern.IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+                new IdentityResource[]
+                {
+                        new IdentityResources.OpenId(),
+                        new IdentityResources.Profile()
+                };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+                new ApiScope[]
+                {
+                    new ApiScope("test-api", "Test Api")
+                };
+
+        public static IEnumerable<Client> Clients =>
+                new Client[]
+                { };
+    }
+}
---
+++ src/Tavern.IdentityServer/Startup.cs
@@ -0,0 +1,59 @@
+using IdentityServerHost.Quickstart.UI;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tavern.IdentityServer
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // uncomment, if you want to add an MVC-based UI
+            services.AddControllersWithViews();
+
+            var builder = services.AddIdentityServer(options =>
+            {
+                // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
+                options.EmitStaticAudienceClaim = true;
+            })
+                .AddInMemoryIdentityResources(Config.IdentityResources)
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddInMemoryClients(Config.Clients)
+                .AddTestUsers(TestUsers.Users);
+
+            // not recommended for production - you need to store your key material somewhere secure
+            builder.AddDeveloperSigningCredential();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // uncomment if you want to add MVC
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseIdentityServer();
+
+            // uncomment, if you want to add MVC
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
